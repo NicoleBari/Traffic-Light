586c9a1d0aec83adc71317736726603762016874
NicoleBari <nicole.barisich.l@gmail.com>
2022-07-18 22:11:54 +0000

    Traffic Light

---
+++ src/js/component/semaforo.jsx
@@ -0,0 +1,35 @@
+
+import React, { useState } from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const  Semaforo = () =>  {
+  const [selectedColor, setSelectedColor] = useState("");
+
+  return (
+    <div className= "fondo">
+    <div>
+      <div className= "stick"></div>
+    <div className="traffic-light">
+      
+      <div
+        onClick={() => setSelectedColor("red")}
+        className={"light red" + (selectedColor === "red" ? " glow" : "")}
+      ></div>
+      <div
+        onClick={() => setSelectedColor("yellow")}
+        className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}
+      ></div>
+      <div
+        onClick={() => setSelectedColor("green")}
+        className={"light green" + (selectedColor === "green" ? " glow" : "")}
+      ></div>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default Semaforo;
